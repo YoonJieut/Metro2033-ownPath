45aa7eb3273e8118661d1641472bb3481567fc97
Yoonjieut <qmdldos@gmail.com>
2023-10-02 23:35:21 +0900

    html, css 기본 구성 완성

---
+++ custom.js
@@ -0,0 +1,45 @@
+// !!!!대화 텍스트 
+const _textBook = [];
+const _textArtium = [];
+const _textSergey = [];
+const _textYeof = [];
+// !!!!등장인물
+const _person = [
+  { id : 1,
+    img : null,
+    name : "아르티옴",
+    장비 : "없음"
+  },
+  { id : 2,
+    img : null,
+    name : "세르게이",
+    장비 : "경비병 장비와 7미리 소총",
+  },
+  { id : 3,
+    img : null,
+    name : "예프게니",
+    장비 : "경비병 장비와 7미리 소총"
+  },
+]
+const _animal = [
+  { id : 1,
+    img : null,
+    name : "고양이",
+    sub : "세르게이가 데리고 있던 고양이 온순함, 쓰다듬고 싶음"
+  },
+]
+
+
+//! 대화(#container) 창 종료 함수
+
+// ! 대화(#container) 창 생성 로직과 함수
+
+
+// ! 해설 대사 함수
+
+
+// ! 주인공 대사 함수
+// * .textBox>.profi+.text
+
+// ! npc 대사 
+// * .textBox>.text+.profi
