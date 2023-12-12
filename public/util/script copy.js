9ad7e553683563ec251bc359e9032cb1c1ff92bc
YoonJieut <qmdldos@gmail.com>
2023-12-13 01:40:57 +0900

    확인 : 아하! 참조값을 동일하게 해야 컨트롤이 가능하구나, 그렇다면? 0으로 초기화하는 로직도 작성 가능할까?

--- public/util/script copy.js
+++ public/util/script copy.js
@@ -2,13 +2,13 @@
 
 function closureTest (){
     let count = 0;
-    console.log(count);
     return function(){
         count++
         return count
     }
 }
-
-closureTest();
-closureTest();
-closureTest();
+const a = closureTest();
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
