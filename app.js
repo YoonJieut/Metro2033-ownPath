46c9dbd9b896521aa2654794be159c6b24c6e363
YoonJieut <qmdldos@gmail.com>
2023-12-26 20:40:10 +0900

    fix : xhr -> express 방식으로 수정

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/index.html') {
-        fs.readFile('./public/index.html', 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        });
-    } else if (req.url.endsWith('.css')) {
-        fs.readFile(`./public${req.url}`, 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Not Found');
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/css' });
-            res.end(data);
-        });
-    } else if (req.url.endsWith('.js')) {
-        fs.readFile(`./public${req.url}`, 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Not Found');
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/javascript' });
-            res.end(data);
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(PORT, () => {
+app.get('/*.css', (req, res) => {
+    res.sendFile(__dirname + '/public' + req.url);
+});
+
+app.get('/*.js', (req, res) => {
+    res.sendFile(__dirname + '/public' + req.url);
+});
+
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
+
+app.listen(PORT, () => {
     console.log(`Server 돌아가는 중 http://localhost:${PORT}`);
 });
